6f306f57f2de83440d40df08bfcbaea306292e5e
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-13 14:04:18 +0530

    first commit

---
+++ myapp/src/Components/Hospital.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Kumta from './Kumta';
+
+function Hospital() {
+  return (
+    <div>
+      <h1>Hospital Component</h1>
+      <Kumta />
+    </div>
+  );
+}
+
+export default Hospital;
